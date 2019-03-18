65269f58d6bdb67884f03b9fd4c05dab1b6ecfae
DanGSun <DanGSun@yandex.ru>
2019-03-18 19:48:00 +0300

    TOML Config. Part #2.0.2. Integration #1, Fix #2

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(
     description='HODL network',
     author='hodleum',
     packages=find_packages(),
+    package_data={'': ['config/*.toml']},
+    include_package_data=True,
     install_requires=['twisted', 'werkzeug', 'attrs', 'pycryptodome', 'sqlalchemy', 'toml']
 )
